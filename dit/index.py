d19790fa14881b8b485274ac9e709e8264d30a88
Derek Anderson <public@kered.org>
2014-10-15 12:47:31 -0500

    index and display author information

--- dit/index.py
+++ dit/index.py
@@ -6,6 +6,30 @@ class Index(object):
 
   def __init__(self):
     self.root = find_root(os.getcwd())
+    self._issues_by_id = {uuid.UUID(issue['id']):issue for issue in self._load_issues()}
+    self._comments_by_id = {uuid.UUID(comment['id']):comment for issue in self._issues_by_id.values() for comment in issue['_comments']}
+    self.index_history()
+  
+  def index_history(self):
+    def index(commit, diff, blob):
+      if blob is None: return
+      if not blob.path.endswith('.json'): return
+      o = json.load(blob.data_stream)
+      uid = uuid.UUID(o['id'])
+      author = str(commit.author)
+      authors_list = self._issues_by_id[uid]['_authors'] if uid in self._issues_by_id else None
+      print uid, author
+      if uid in self._comments_by_id:
+        authors_list = self._comments_by_id[uid]['_authors']
+      if authors_list is not None and author not in authors_list:
+        authors_list.append(author)
+    project_dir = os.path.dirname(self.root)
+    repo = git.Repo(project_dir)
+    for commit in repo.iter_commits():
+      for diff in commit.diff():
+        index(commit, diff, diff.a_blob)
+        index(commit, diff, diff.b_blob)
+    
   
   def repo(self):
     project_dir = os.path.dirname(self.root)
@@ -19,9 +43,9 @@ class Index(object):
     }
     
   def issues(self):
-    return [issue for issue,path in self._issues()]
+    return self._issues_by_id.values()
     
-  def _issues(self):
+  def _load_issues(self):
     issues_dir = os.path.join(self.root,'issues')
     for fn in os.listdir(issues_dir):
       issue_path = os.path.join(issues_dir,fn)
@@ -29,30 +53,29 @@ class Index(object):
         with open(os.path.join(issue_path,'issue.json'),'r') as f:
           issue = json.load(f)
           issue['_comments'] = []
+          issue['_authors'] = []
+          issue['__path'] = issue_path
           for cfn in os.listdir(os.path.join(issue_path,'comments')):
             cfn = os.path.join(issue_path,'comments',cfn)
             with open(cfn,'r') as cf:
               comment = json.load(cf)
+              comment['_authors'] = []
               comment['_text'] = bleach.clean(markdown.markdown(comment['text']))
               issue['_comments'].append(comment)
-          yield issue, issue_path
+          yield issue
   
   def issue(self, uid):
-    uid = str(uid)
-    for issue, issue_path in self._issues():
-      if issue['id'] == uid:
-        return issue
+    return self._issues_by_id[uid]
   
   def _issue_path(self, uid):
-    uid = str(uid)
-    for issue, issue_path in self._issues():
-      if issue['id'] == uid:
-        return issue_path
+    uid = uuid.UUID(uid)
+    return self._issues_by_id[uid]['__path']
   
   def save_issue(self, issue):
-    uuid.UUID(issue['id'])
+    uid = uuid.UUID(issue['id'])
     fn = os.path.join(self._issue_path(issue['id']),'issue.json')
-    save(issue, fn)
+    issue = save(issue, fn)
+    self._issues_by_id[uid].update(issue)
   
   def _get_comment_path(self, comment):
     comment_path = os.path.join(self._issue_path(comment['issue_id']),'comments')
@@ -78,13 +101,14 @@ class Index(object):
 
 
 def save(o, fn):
+  o = o.copy()
   with open(fn,'w') as f:
-    o = o.copy()
     for k in o.keys():
       if k.startswith('_'):
         del o[k]
     print 'writing', fn
     json.dump(o,f, sort_keys=True, indent=4)
+  return o
 
 def find_root(path):
   candidate = os.path.join(path,'.dit')
