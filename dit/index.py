c89ae7dfc872da3ed3b671691f7fd88f8aecc539
Derek Anderson <public@kered.org>
2014-10-17 12:13:05 -0500

    -

--- dit/index.py
+++ dit/index.py
@@ -1,4 +1,4 @@
-import bleach, collections, git, json, markdown, os, re, traceback, uuid
+import bleach, collections, git, json, markdown, os, re, time, traceback, uuid
 
 bleach.ALLOWED_TAGS.append('p')
 
@@ -6,10 +6,16 @@ class Index(object):
 
   def __init__(self):
     self.root = find_root(os.getcwd())
+    print 'dit root:', self.root
     self._comments_by_id = {}
     self._comment_ids_by_issue_id = collections.defaultdict(set)
+    self._commits_by_issue_id = collections.defaultdict(list)
     self._issues_by_id = {uuid.UUID(issue['id']):issue for issue in self._load_issues()}
     self.index_history()
+    print 'len(self._issues_by_id)', len(self._issues_by_id)
+    print 'len(self._comments_by_id)', len(self._comments_by_id)
+    print 'len(self._comment_ids_by_issue_id)', len(self._comment_ids_by_issue_id)
+    print 'len(self._commits_by_issue_id)', len(self._commits_by_issue_id)
   
   def index_history(self):
     def index(commit, diff, blob):
@@ -24,6 +30,7 @@ class Index(object):
         traceback.print_exc()
         print 'could not decode', blob.path
       author = str(commit.author)
+      print 'author', author
       original = None
       if uid in self._issues_by_id: original = self._issues_by_id[uid]
       if uid in self._comments_by_id: original = self._comments_by_id[uid]
@@ -31,13 +38,39 @@ class Index(object):
       if original is not None:
         if author not in original['_authors']:
           original['_authors'].append(author)
+        original['_commit_id'] = commit.hexsha
         original['_committed_on'] = commit.committed_date
     project_dir = os.path.dirname(self.root)
     repo = git.Repo(project_dir)
-    for commit in repo.iter_commits():
-      for diff in commit.diff():
-        index(commit, diff, diff.a_blob)
-        index(commit, diff, diff.b_blob)
+    try:
+      c = 0
+      for commit in repo.iter_commits():
+        c += 1
+        print commit.message
+        files = commit.stats.files
+        for match in ISSUE_ID_RE.findall(commit.message):
+          print 'match', match
+          issue_id = uuid.UUID(match[1:])
+          self._commits_by_issue_id[issue_id].append({
+            'hexsha': commit.hexsha,
+            '_committed_on': commit.committed_date,
+            'files': files,
+            'message': commit.message,
+            'type': 'commit',
+          })
+        for o in commit.tree.traverse():
+          if not isinstance(o, git.objects.blob.Blob): continue
+          if o.path not in files: continue
+          index(commit, None, o)
+          print o.path
+
+      print 'indexed', c, 'commits'
+    except ValueError as e:
+      # message: Reference at 'refs/heads/master' does not exist
+      # this happens when a repo has no commits (brand new after init)
+      print e
+      pass
+      
     
   
   def repo(self):
@@ -61,6 +94,7 @@ class Index(object):
     
   def _load_issues(self):
     issues_dir = os.path.join(self.root,'issues')
+    if not os.path.isdir(issues_dir): return
     for fn in os.listdir(issues_dir):
       issue_path = os.path.join(issues_dir,fn)
       issue_fn = os.path.join(issue_path,'issue.json')
@@ -87,6 +121,7 @@ class Index(object):
   def issue(self, uid):
     issue = self._issues_by_id[uid]
     comments = [self._comments_by_id[cid] for cid in self._comment_ids_by_issue_id[uuid.UUID(issue['id'])]]
+    comments += self._commits_by_issue_id[uid]
     comments.sort(cmp_comments)
     print '\n'.join([str(c) for c in comments])
     issue['_comments'] = comments
@@ -107,6 +142,8 @@ class Index(object):
       uid = uuid.uuid4()
       issue['id'] = str(uid)
       issues_dir = os.path.join(self.root,'issues')
+      if not os.path.isdir(issues_dir):
+        os.mkdir(issues_dir)
       issue_dir = '%s--%s' % (slugify(issue['title']), issue['id'])
       issue_path = os.path.join(issues_dir, issue_dir)
       os.mkdir(issue_path)
@@ -163,7 +200,11 @@ def find_root(path):
   if os.path.isdir(candidate):
     return candidate
   if path=='/': return None
-  return os.path.dirname(path)
+  if os.path.isdir(os.path.join(path,'.git')) or os.path.isdir(os.path.join(path,'.hg')):
+    os.mkdir(candidate)
+    return candidate
+  if os.path.isdir(os.path.join(path,'.hg')): return None
+  return find_root(os.path.dirname(path))
     
     
 def slugify(s):
@@ -177,4 +218,6 @@ def get_timestamps(o):
 def cmp_comments(a,b):
   return cmp(get_timestamps(a), get_timestamps(b))
   
-  
+
+ISSUE_ID_RE = re.compile('#[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}')
+
