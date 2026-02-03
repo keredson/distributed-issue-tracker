b21e0fe8279f6cc0166695297b42c7ffd45b6249
Derek Anderson <public@kered.org>
2026-02-02 17:14:48 -0800

    so fresh and so clean clean...

--- dit/__main__.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-if __name__=='__main__':
-  if sys.argv[1]=='serve':
-    import server
-    server.serve()
--- dit/jsx/index.jsx
+++
@@ -1,7 +0,0 @@
-React.render(
-  <Frame>
-    <h1>Distributed Issue Tracker</h1>
-  </Frame>,
-  document.getElementById('content')
-);
-
